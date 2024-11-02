ec2c38e00a90a7f815edca551654aa64c6cc4d30
HIENLTH 2023 <hyhien@gmail.com>
2024-11-02 08:17:07 +0700

    Chi tiet hang hoa

--- MyEStore/MyEStore/Controllers/ProductsController.cs
+++ MyEStore/MyEStore/Controllers/ProductsController.cs
@@ -4,26 +4,39 @@ using MyEStore.Models;
 
 namespace MyEStore.Controllers
 {
-	public class ProductsController : Controller
-	{
-		private readonly MyeStoreContext _ctx;
+    public class ProductsController : Controller
+    {
+        private readonly MyeStoreContext _ctx;
 
-		public ProductsController(MyeStoreContext ctx)
-		{
-			_ctx = ctx;
-		}
-		public IActionResult Index(int? cateId)
-		{
-			var data = _ctx.HangHoas.AsQueryable();
-			if (cateId.HasValue)
-			{
-				data = data.Where(hh => hh.MaLoai == cateId.Value);
-			}
-			var result = data.Select(hh => new HangHoaVM {
-				MaHh = hh.MaHh, TenHh = hh.TenHh,
-				DonGia = hh.DonGia ?? 0, Hinh = hh.Hinh
-			}).ToList();
-			return View(result);
-		}
-	}
+        public ProductsController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IActionResult Index(int? cateId)
+        {
+            var data = _ctx.HangHoas.AsQueryable();
+            if (cateId.HasValue)
+            {
+                data = data.Where(hh => hh.MaLoai == cateId.Value);
+            }
+            var result = data.Select(hh => new HangHoaVM
+            {
+                MaHh = hh.MaHh,
+                TenHh = hh.TenHh,
+                DonGia = hh.DonGia ?? 0,
+                Hinh = hh.Hinh
+            }).ToList();
+            return View(result);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var data = _ctx.HangHoas.SingleOrDefault(hh => hh.MaHh == id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return View(data);
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ec21bitv03
 Demo website TMĐT lớp 21BITV03
+* LAB: https://bit.ly/lab-tmdt 
+* Source: https://github.com/niie-it/ec21bitv03
+
+# Day01
+- Dựng project
+- Xây dựng trang Danh sách
+- Xây dựng menu Chủng loại (ViewComponent)
+
+# Day02
+- Trang chi tiết (nút mua kèm số lượng)
+- Giỏ hàng (cho phép cập nhật số lượng)
+- Đăng ký, đăng nhập, phân quyền
